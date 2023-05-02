14b956b593030866209be7d686f42c7b6098db69
TacLabs <tac.labs.lol@protonmail.com>
2023-05-02 22:20:55 +0200

    PostalNVSE début

---
+++ PostalNVSE/main.cpp
@@ -0,0 +1,131 @@
+#include "nvse/PluginAPI.h"
+#include "nvse/CommandTable.h"
+#include "nvse/GameAPI.h"
+#include "nvse/ParamInfos.h"
+#include "nvse/GameObjects.h"
+#include <string>
+//NoGore is unsupported in xNVSE
+
+
+// RUNTIME = Is not being compiled as a GECK plugin.
+#if RUNTIME
+NVSEScriptInterface* g_script{};
+NVSEStringVarInterface* g_stringInterface{};
+NVSEArrayVarInterface* g_arrayInterface{};
+NVSEDataInterface* g_dataInterface{};
+NVSESerializationInterface* g_serializationInterface{};
+NVSEConsoleInterface* g_consoleInterface{};
+NVSEEventManagerInterface* g_eventInterface{};
+bool (*ExtractArgsEx)(COMMAND_ARGS_EX, ...);
+#endif
+
+/****************
+ * Here we include the code + definitions for our script functions,
+ * which are packed in header files to avoid lengthening this file.
+ * Notice that these files don't require #include statements for globals/macros like ExtractArgsEx.
+ * This is because the "fn_.h" files are only used here,
+ * and they are included after such globals/macros have been defined.
+ ***************/
+
+
+// Shortcut macro to register a script command (assigning it an Opcode).
+#define RegisterScriptCommand(name) 	nvse->RegisterCommand(&kCommandInfo_ ##name)
+
+// Short version of RegisterScriptCommand.
+#define REG_CMD(name) RegisterScriptCommand(name)
+
+// Use this when the function's return type is not a number (when registering array/form/string functions).
+//Credits: taken from JohnnyGuitarNVSE.
+#define REG_TYPED_CMD(name, type)	nvse->RegisterTypedCommand(&kCommandInfo_##name,kRetnType_##type)
+
+bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* info)
+{
+	_MESSAGE("query");
+
+	// fill out the info structure
+	info->infoVersion = PluginInfo::kInfoVersion;
+	info->name = "PostalNVSE";
+	info->version = 2;
+
+	// version checks pass
+	// any version compatibility checks should be done here
+	return true;
+}
+
+bool NVSEPlugin_Load(NVSEInterface* nvse)
+{
+	_MESSAGE("load");
+
+	
+
+	if (!nvse->isEditor)
+	{
+#if RUNTIME
+		// script and function-related interfaces
+		g_script = static_cast<NVSEScriptInterface*>(nvse->QueryInterface(kInterface_Script));
+		g_stringInterface = static_cast<NVSEStringVarInterface*>(nvse->QueryInterface(kInterface_StringVar));
+		g_arrayInterface = static_cast<NVSEArrayVarInterface*>(nvse->QueryInterface(kInterface_ArrayVar));
+		g_dataInterface = static_cast<NVSEDataInterface*>(nvse->QueryInterface(kInterface_Data));
+		g_eventInterface = static_cast<NVSEEventManagerInterface*>(nvse->QueryInterface(kInterface_EventManager));
+		g_serializationInterface = static_cast<NVSESerializationInterface*>(nvse->QueryInterface(kInterface_Serialization));
+		g_consoleInterface = static_cast<NVSEConsoleInterface*>(nvse->QueryInterface(kInterface_Console));
+		ExtractArgsEx = g_script->ExtractArgsEx;
+#endif
+	}
+	
+
+	/***************************************************************************
+	 *
+	 *	READ THIS!
+	 *
+	 *	Before releasing your plugin, you need to request an opcode range from
+	 *	the NVSE team and set it in your first SetOpcodeBase call. If you do not
+	 *	do this, your plugin will create major compatibility issues with other
+	 *	plugins, and will not load in release versions of NVSE. See
+	 *	nvse_readme.txt for more information.
+	 *
+	 *	See https://geckwiki.com/index.php?title=NVSE_Opcode_Base
+	 *
+	 **************************************************************************/
+
+	// Do NOT use this value when releasing your plugin; request your own opcode range.
+	UInt32 const examplePluginOpcodeBase = 0x6969;
+	
+	 // register commands
+	nvse->SetOpcodeBase(examplePluginOpcodeBase);
+	
+	/*************************
+	 * The hexadecimal Opcodes are written as comments to the left of their respective functions.
+	 * It's important to keep track of how many Opcodes are being used up,
+	 * as each plugin is given a limited range which may need to be expanded at some point.
+	 *
+	 * === How Opcodes Work ===
+	 * Each function is associated to an Opcode,
+	 * which the game uses to look-up where to find your function's code.
+	 * It is CRUCIAL to never change a function's Opcode once it is released to the public.
+	 * This is because when compiling a script, each function being called are represented as Opcodes.
+	 * So changing a function's Opcode will invalidate previously compiled scripts,
+	 * as they will fail to look up that function properly, instead probably finding some other function.
+	 *
+	 * Example: say we compile a script that uses ExamplePlugin_IsNPCFemale.
+	 * The compiled script will check for the Opcode 0x2002 to call that function, and should work fine.
+	 * If we remove /REG_CMD(ExamplePlugin_CrashScript);/, and don't register a new function to replace it,
+	 * then `REG_CMD(ExamplePlugin_IsNPCFemale);` now registers with Opcode #0x2001.
+	 * When we test the script now, a bug/crash is bound to happen,
+	 * since the script is looking for an Opcode which is no longer bound to the expected function.
+	 ************************/
+	
+	/*
+     RegisterScriptCommand(ExamplePlugin_PluginTest);
+	 REG_CMD(ExamplePlugin_CrashScript);
+	 REG_CMD(ExamplePlugin_IsNPCFemale);
+	 REG_CMD(ExamplePlugin_FunctionWithAnAlias);
+	 REG_TYPED_CMD(ExamplePlugin_ReturnForm, Form);
+	 REG_TYPED_CMD(ExamplePlugin_ReturnString, String);	// ignore the highlighting for String class, that's not being used here.
+	REG_TYPED_CMD(ExamplePlugin_ReturnArray, Array);
+	*/
+
+
+
+	return true;
+}
