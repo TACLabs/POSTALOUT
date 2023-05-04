571772891f4f7956c84da074cb34afb203ac4e9e
TacLabs <tac.labs.lol@protonmail.com>
2023-05-04 22:07:38 +0200

    KICK.dll en cours de construction

--- KICK/main.cpp
+++ KICK/main.cpp
@@ -47,31 +47,85 @@ __declspec(naked) UInt32 __fastcall HexToUInt(const char* str)
 	}
 }
 
+#define CALL_EAX(addr) __asm mov eax, addr __asm call eax
+
+__declspec(naked) TESIdleForm* AnimData::GetPlayedIdle()
+{
+	__asm
+	{
+		mov		eax, [ecx + 0x128]
+		test	eax, eax
+		jz		noQueued
+		mov		eax, [eax + 0x2C]
+		test	eax, eax
+		jnz		done
+		noQueued :
+		mov		eax, [ecx + 0x124]
+			test	eax, eax
+			jz		done
+			mov		eax, [eax + 0x2C]
+			test	eax, eax
+			jz		done
+			push	eax
+			CALL_EAX(0x4985F0)
+			pop		edx
+			movzx	eax, al
+			dec		eax
+			and eax, edx
+			done :
+		retn
+	}
+}
+
+bool __fastcall IsPlayerIdlePlaying(TESIdleForm* idleAnim)
+{
+	PlayerCharacter* pc = PlayerCharacter::GetSingleton();
+	//AnimData* animData = thisObj->GetAnimData();
+	AnimData* animData3rd = pc->baseProcess->GetAnimData();
+	AnimData* animData1st = pc->firstPersonAnimData;
+	return (animData3rd && (animData3rd->GetPlayedIdle() == idleAnim)) || (animData1st && (animData1st->GetPlayedIdle() == idleAnim));
+}
+
 
 bool init = false;
 
 PlayerCharacter* pc = 0;
-
 DataHandler* dh = 0;
 
-
 TESIdleForm* kickAnim;
-const char* modName = "PostalKICK.esp";
-const char* kickAnimIndex = "DBB";
+const char* modName = "B42Bash.esp";
+const char* kickAnimIndex = "800";
 UInt32 kickAnimRefID = 0;
 
 
-void KickPanardInclinaison(NVSEMessagingInterface::Message* msg)
+void KickPanardGestion(NVSEMessagingInterface::Message* msg)
 {
-
 	switch (msg->type)
 	{
 	case NVSEMessagingInterface::kMessage_MainGameLoop:
 		if (init)
 		{
-			if (!(pc->bThirdPerson) )
+			//ALERTE : ON FAIT UN KICK
+			if (!(pc->bThirdPerson) && IsPlayerIdlePlaying(kickAnim))
 			{
-				Console_Print("OK");
+				// Le corps de la troisième personne n'est pas visible
+				if ()
+				{
+					//Bah on l'affiche
+				}
+				else
+				{
+					//On fait notre tambouille avec le calcul puis l'inclinaison du body pour voir le panard en toutes circonstances
+
+				}
+			}
+			else
+			{
+			//Si on est toujours en première personne et que le corps de la troisième personne est toujours visible
+				if (!(pc->bThirdPerson)&&)
+				{
+					//Bah on le désaffiche
+				}
 			}
 		}
 		break;
@@ -130,7 +184,7 @@ bool NVSEPlugin_Load(NVSEInterface* nvse)
 
 	// register to receive messages from NVSE
 	g_messagingInterface = static_cast<NVSEMessagingInterface*>(nvse->QueryInterface(kInterface_Messaging));
-	g_messagingInterface->RegisterListener(g_pluginHandle, "NVSE", KickPanardInclinaison);
+	g_messagingInterface->RegisterListener(g_pluginHandle, "NVSE", KickPanardGestion);
 
 	// Disables a jump that prevents the 1st person body 
 	// from being visible if the 3rd person body is visible
