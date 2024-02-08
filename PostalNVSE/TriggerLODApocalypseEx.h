8fd5ccc2ec12de0be42701f037b99313eedbde94
TACLabs <tac.labs.lol@protonmail.com>
2024-02-08 15:06:29 +0100

    Another milestone : J'ai réussi à REMPLACER une fonction entière du jeu par la MIENNE

--- PostalNVSE/TriggerLODApocalypseEx.h
+++ PostalNVSE/TriggerLODApocalypseEx.h
@@ -1,8 +1,43 @@
 #pragma once
 
+int functionCall = 0x406D00;
+__declspec(naked) int TambouilleduLOD()
+{
+	_asm {
+		push ebp
+		mov ebp, esp
+		movsx eax,word ptr [ebp+0x18]
+		push eax
+		movsx ecx,word ptr [ebp+0x14]
+		push ecx
+		mov edx,dword ptr [ebp+0x10]
+		push edx
+		mov eax, dword ptr[ebp + 0xC]
+		mov edx, [eax]
+		mov ecx, dword ptr[ebp + 0xC]
+		mov eax, DWORD PTR[edx + 0x130]
+		call eax
+		push eax
+		mov ecx, dword ptr[ebp + 0xC]
+		mov edx,[ecx]
+		mov ecx, dword ptr[ebp + 0xC]
+		mov eax, DWORD PTR[edx + 0x130]
+		call eax
+		push eax
+		push 0x106daf8
+		push 0x104
+		mov  ecx, DWORD PTR[ebp + 0x8]
+		push ecx
+		call functionCall
+		add esp,0x20
+		pop ebp
+		retn
+	}
+}
+
+
 bool Cmd_TriggerLODApocalypseEx_Execute(COMMAND_ARGS) {
 	char suffix[MAX_PATH];
-	bool state = true;
 	*result = 0;
 	if (ExtractArgsEx(EXTRACT_ARGS_EX, &suffix)) {
 		std::string sSuffix = suffix;
@@ -20,13 +55,12 @@ bool Cmd_TriggerLODApocalypseEx_Execute(COMMAND_ARGS) {
 	return true;
 }
 
-
+/*
 void hann()
 {
 	Console_Print("LOAD");
 }
 
-/*
 int retourTest1 = 0x6F6F00;
 int doOnce = 0;
 __declspec(naked) void test1()
@@ -46,5 +80,8 @@ __declspec(naked) void test1()
 void TriggerLODApocalypseExHooks()
 {
 	//WriteRelJump(0x6F6E63, (UInt32)test1);		//PETIT TEST
-	//WriteRelJump(0x6F6E63, 0x6F6E99);				LOAD LE POSTAPOCALYPSE LOAD, BIEN COMME IL FAUT, DES LE LANCEMENT DU JEU
+
+	WriteRelJump(0x6F6E63, 0x6F6E99);				//LOAD LE POSTAPOCALYPSE LOAD, BIEN COMME IL FAUT, DES LE LANCEMENT DU JEU
+	WriteRelCall(0x6F6EB8, (UInt32)TambouilleduLOD); //AI REUSSI A REMPLACER UNE FONCTION DANS LE SOURCE CODE DU JEU
+    
 }
